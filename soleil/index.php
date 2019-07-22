cad3dcebfb00c32fec744d41a6462a322a7631f0
Mohamed Zaki <52196443+Mohamedzaki-eng@users.noreply.github.com>
2019-07-22 14:45:24 -0400

    First Commit

---
+++ soleil/index.php
@@ -0,0 +1,134 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+  <!-- Latest compiled and minified CSS -->
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+  <!-- Style -->
+  <link rel="stylesheet" href="style.css">
+  <!-- FontAwesome-->
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.1.0/css/all.css"> 
+
+    <title>Soleil</title>
+
+</head>
+<body>
+    <!-- nav bar -->
+
+    <nav class="navbar navbar-inverse">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span> 
+          </button>
+          <a class="navbar-brand" href="#"><span style = "font-family: 'EB Garamond', serif; font-size:50px;"> Soleil </span> Tallahassee, FL</a>
+        </div>
+        <div class="collapse navbar-collapse">
+          <ul class="nav navbar-nav navbar-right">
+            <li><a href="#"><span class="glyphicon glyphicon-home"></span> Home</a></li>
+            <li><a href="#"><span class="glyphicon glyphicon-user"></span> Reservations</a></li>
+            <li><a href="#"><span class="glyphicon glyphicon-phone"></span> Contact Us</a></li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+
+    <!-- header -->
+
+    <div class = "container">
+        <div class = "row"> 
+            <div class = "col-md-12 text-center"> 
+              <div class="bg"></div>  
+              <br><br>
+              <p style= "letter-spacing: 10px; font-size:30px; color:white;">Voted Best New Restaurant in Tallahassee FL </p>
+              <br><br>
+              <button style = "border-radius: 20px;">Order online</button>
+            </div>
+        </div>
+    </div>
+
+    <!-- our menus -->
+    <div class = "container-fluid" style = "padding-top: 70px; padding-bottom: 80px;">
+      <div class = "row">
+        <div class = "col-md-12 text-center"><h3 style= "color: white"> Our Menus</h3><br>
+        </div>
+      </div>
+    </div>
+
+      <div class = "row" style="margin-right: 10px;"> 
+            <div class = "col-md-4 text-center">
+              <img src="imgs/breakfast.jpg" style = "border:2px solid brown; height: 200px; width: 400px; opacity: .9;border-radius:10%;"alt="BreakFast"><br><br> 
+              <button class ="btn btn-danger">Breakfast</button>
+            </div>
+
+            <div class = "col-md-4 text-center">
+              <img src="imgs/dinner.jpg"style = "border:2px solid brown; width:90%; opacity: .9;border-radius:10%;"alt="Lunch"><br><br>
+              <button class="btn btn-danger">Lunch</button>
+            </div>
+
+            <div class = "col-md-4 text-center"><img src="imgs/lunch.jpg"style = "border:2px solid brown; width:60%; height: 200px; width: 420px; opacity: .9;border-radius:10%;"alt="Dinner"><br><br><button class = "btn btn-danger"> Dinner </button></div>
+      </div>
+
+    <!-- Our chef and reviews -->
+    <div class = "container-fluid" style = "padding-top: 100px;">
+      <div class = "row">
+        <div class = "col-md-6 text-center"><button class = "button">Our Chef</button> <br> <br></div>
+        <div class = "col-md-6 text-center"><button class = "button">Reviews</div>
+      </div>
+    </div>
+    
+    <div class = "row" style = "color: white;">
+       <div class = "col-md-6 text-center" style = "display: flex; flex-direction:column; justify-content: center; align-items: center;">
+              <h3>Our Chef: <strong>Gabriel Jules</strong></h3><br> 
+              <p>In May 2016, chef Jules joined <strong>Soleil Restaurants</strong> Jules is a graduate of Alexandria University, where he received the Escoffier Award for Culinary and Academic Excellence. He continues his work as a frequent teacher, guest speaker, and Culinary Advisory Board Member. A French native, Jules enjoys competing in triathlons and cooking for friends and family. </p>
+              <!-- chef image -->
+              <img src = "imgs/chef.jpg" class="img-thumbnail" style="padding:1px;width:200px;float:right;"><br> <br> <br>
+              <img src="imgs/socialicons.jpg" style = "border:2px solid brown; width:40%; opacity: .6;border-radius:10%; "alt="social_icons"><br>
+       </div>
+             <!-- Comments -->
+       <div class = "col-md-6"> 
+              <i class="fas fa-star" style="color:gold;text-shadow: 1px 1px black;font-size:18px;"></i>
+              <i class="fas fa-star" style="color:gold;text-shadow: 1px 1px black;font-size:18px;"></i>
+              <i class="fas fa-star" style="color:gold;text-shadow: 1px 1px black;font-size:18px;"></i>
+              <i class="fas fa-star" style="color:gold;text-shadow: 1px 1px black;font-size:18px;"></i>
+              <i class="fas fa-star" style="color:gold;text-shadow: 1px 1px black;font-size:18px;"></i> 
+              "Soleil is my favorite restaurant. A nice experience again at this location! The restaurant looked very organized and clean. Food was AMAZING. We'll absolutely come back!."<br><span style="color:#30a64a;font-size:16px;float:right;">--Ronny Jawerd</span> <br><br>
+
+              <i class="fas fa-star" style="color:gold;text-shadow: 1px 1px black;font-size:18px;"></i>
+              <i class="fas fa-star" style="color:gold;text-shadow: 1px 1px black;font-size:18px;"></i>
+              <i class="fas fa-star" style="color:gold;text-shadow: 1px 1px black;font-size:18px;"></i>
+              <i class="fas fa-star" style="color:gold;text-shadow: 1px 1px black;font-size:18px;"></i>
+              <i class="fas fa-star" style="color:gold;text-shadow: 1px 1px black;font-size:18px;"></i> 
+              "High class attention to detail, and the finest dining. Wonderful insight from our server made for a perfect pairing on all courses.!." <br><span style="color:#30a64a;font-size:16px;float:right;">--Lisa Mcnolasies</span> <br><br>
+         
+              <i class="fas fa-star" style="color:gold;text-shadow: 1px 1px black;font-size:18px;"></i>
+              <i class="fas fa-star" style="color:gold;text-shadow: 1px 1px black;font-size:18px;"></i>
+              <i class="fas fa-star" style="color:gold;text-shadow: 1px 1px black;font-size:18px;"></i>
+              <i class="fas fa-star" style="color:gold;text-shadow: 1px 1px black;font-size:18px;"></i>
+              <i class="fas fa-star" style="color:gold;text-shadow: 1px 1px black;font-size:18px;"></i> 
+              "With that amazing buttery, creamy, silky, smooth sauce.. cooked to a perfect medium rare.. with their special blend of coffee rub and spice.. soooo good .. just pricy. Amazing service and a wine selection to go along with your meal to make it just that more special." <br><span style="color:#30a64a;font-size:16px;float:right;">--Elizabeth Bantariile</span><br><br>
+
+              <i class="fas fa-star" style="color:gold;text-shadow: 1px 1px black;font-size:18px;"></i>
+              <i class="fas fa-star" style="color:gold;text-shadow: 1px 1px black;font-size:18px;"></i>
+              <i class="fas fa-star" style="color:gold;text-shadow: 1px 1px black;font-size:18px;"></i>
+              <i class="fas fa-star" style="color:gold;text-shadow: 1px 1px black;font-size:18px;"></i>
+              <i class="fas fa-star" style="color:gold;text-shadow: 1px 1px black;font-size:18px;"></i> 
+              "Great service!!! Great customer service!!! Food was delicious!!! Very upscale restaurant!!! And it was my friend's birthday and she loved it!!! I will come again!." <br><span style="color:#30a64a;font-size:16px;float:right;">--Chase Family</span>
+       </div>
+    </div>
+
+    <!-- Contact info bar -->
+    <div class = "container-fluid text-center" style = "color:#e0dbdb; font-weight: bold; font-size: 20px; margin-bottom: 30px;">
+      <div class = "row">
+        <div class = "col-md-4"> Copyright 2019 Soleil, LLC </div>
+        <div class = "col-md-4"> Soleil 2711 E 15th st, Tallahassee, FL 32312 </div>
+        <div class = "col-md-4"> Privacy | Terms of Conditions </div>
+      </div>
+    </div>
+
+    </body>
+</html>
+
+
